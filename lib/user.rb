cbd950b3e8c8c5d6b16fbc6a41e895aebe15b713
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-12 15:57:30 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,14 @@
 class User
   attr_accessor :first_name, :last_name
   
+  @@all = []
+  
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
+  end
+  
+  def self.all
+    @@all
   end
 end
